a3f759241c2a27d4fcf6ec27736e9b93b44ce6a5
LegendCoderDev505 <raizhaikal82@example.com>
2026-05-18 11:38:44 +0700

    Vercel

---
+++ api/news.js
@@ -0,0 +1,6 @@
+import news from "../data/news.json";
+
+export default function handler(req, res) {
+  const random = news[Math.floor(Math.random() * news.length)];
+  res.status(200).json(random);
+}
